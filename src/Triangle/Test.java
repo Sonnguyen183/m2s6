248406c3e084dda361ddec94d741ea3ce7e0ff92
okk <okk@MacBook-Pro-cua-okk.local>
2022-09-14 21:43:51 +0700

    son

---
+++ src/Triangle/Test.java
@@ -0,0 +1,19 @@
+package Triangle;
+
+public class Test {
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle();
+        System.out.println(triangle);
+
+        triangle.setSide1(3);
+        triangle.setSide2(4);
+        triangle.setSide3(5);
+        triangle.setColor("Yellow");
+        triangle.setFilled(false);
+        System.out.println(triangle);
+
+        triangle = new Triangle("blue", false, 6, 6, 6 );
+        System.out.println(triangle);
+
+    }
+}
